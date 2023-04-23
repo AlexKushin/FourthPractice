34f6f7a7e0b5f1c8b2d60fd7f824735359d21f5e
AlexKushin <kyshin1993@gmail.com>
2023-04-23 15:46:51 +0200

    idea folder removed

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,15 @@
+
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+k
+;src/main/java/com/shpp/mentoring/okushin/task4/Product.java,c\e\ce821197dad035e73f008eb00c1badde8ff132b4
+o
+?src/main/java/com/shpp/mentoring/okushin/task4/CsvImporter.java,c\9\c984385254fe6ddacd0326172867e90004539eb8
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+M
+dmlCommandForFillingTable.sql,b\b\bbcff2f60556256f06f3c44050066ac048004f6b
+:
+
+stores.csv,e\2\e26b6e6c018e3fe32b82ac4e45332c27fc367b6a
--- src/main/java/com/shpp/mentoring/okushin/task4/App.java
+++ src/main/java/com/shpp/mentoring/okushin/task4/App.java
@@ -3,10 +3,14 @@ package com.shpp.mentoring.okushin.task4;
 import org.apache.commons.lang3.time.StopWatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -24,6 +28,8 @@ public class App {
         StopWatch watch = new StopWatch();
         try {
             SqlExecute.executeSqlScript(url, user, password, "ddlScriptForDataBaseCreating.sql");
+            SqlExecute.executeSqlCommand(url, user, password, "ALTER DATABASE epicentr_repo SET bytea_output = 'escape'");
+            SqlExecute.executeSqlCommand(url, user, password, "ALTER DATABASE epicentr_repo SET client_encoding = 'UTF8'");
 
             CsvImporter.importToDB(url, user, password,
                     "stores.csv", "availability_goods.stores");
@@ -31,7 +37,7 @@ public class App {
                     "types.csv", "availability_goods.types");
             //    SqlExecute.executeSqlCommand(url, user, password, "CREATE INDEX indexType ON availability_goods.types (id);");
             int storesCount = SqlExecute.executeQuerySqlScript(url, user, password, "SELECT count(*) from availability_goods.stores;");
-           // System.out.println(storesCount);
+            // System.out.println(storesCount);
             int typesCount = SqlExecute.executeQuerySqlScript(url, user, password, "SELECT count(*) from availability_goods.types;");
             //System.out.println(typesCount);
             int numberThreads = 10;
@@ -64,25 +70,24 @@ public class App {
                         watch.start();
                         SqlExecute.executeSqlScript(url, user, password, "dmlCommandForFillingTable.sql");
                         watch.stop();
-                        logger.info("Filling products to stores= {}" , watch.getTime() / 1000.0);
+                        logger.info("filling stores speed with products= {}", watch.getTime() / 1000.0);
                         watch.reset();
-                        Scanner scanner = new Scanner(System.in);
-                        String productType = "";
-                        while (!productType.equals("стоп")) {
-                            logger.info("Введіть тип товару: ");
-                            productType = scanner.nextLine();
-                            watch.start();
-                             SqlExecute.executeQuerySqlScript(url, user, password, "sqlCommandsToExecute.sql", productType);
-                            watch.stop();
-                            logger.info("Search store time= {}", watch.getTime() / 1000.0);
-                            watch.reset();
-                        }
-                        scanner.close();
+
+                        String property = System.getProperty("productType");
+                        String productType = new String(property.getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8);
+                        watch.start();
+                        SqlExecute.executeQuerySqlScript(url, user, password, "sqlCommandsToExecute.sql", productType);
+                        watch.stop();
+                        logger.info("Search store time= {}", watch.getTime() / 1000.0);
+                        watch.reset();
+
+
                         return;
                     }
                 }
             }
-        } catch (SQLException e) {
+        } catch (
+                SQLException e) {
             throw new RuntimeException(e);
         }
     }
