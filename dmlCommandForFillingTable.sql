d0325db6cd985734220830f05dcb19d0d79f6290
AlexKushin <kyshin1993@gmail.com>
2023-04-23 18:17:10 +0200

    for first run on amazon postgresql, url changed2

--- dmlCommandForFillingTable.sql
+++ dmlCommandForFillingTable.sql
@@ -1,3 +1,3 @@
-INSERT INTO epicentr_repo.availability_goods.quantity_in_store (store_id, product_id, total)
-SELECT round(random() * 19) + 1 ::int, epicentr_repo.availability_goods.products.id, round(random() * 100) + 1::int
-FROM epicentr_repo.availability_goods.products;
+INSERT INTO availability_goods.quantity_in_store (store_id, product_id, total)
+SELECT round(random() * 19) + 1 ::int, availability_goods.products.id, round(random() * 100) + 1::int
+FROM availability_goods.products;
